2284a8cfae97c4c62c5871274515f046de242a0c
Thomas Habtemariam <84083304+tomhabt@users.noreply.github.com>
2021-10-08 14:47:38 -0400

    conditional rendering done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Nav from './components/Nav/nav';
 import About from './components/About/about';
 import Gallery from './components/Gallery/gallery';
-import PhotoList from './components/PhotoList/photoList';
 import ContactForm from './components/Contact/contact';
 
 function App() {
@@ -17,19 +16,28 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
       <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
+       
+       categories={categories}
+       setCurrentCategory={setCurrentCategory}
+       currentCategory={currentCategory}
+       contactSelected={contactSelected}
+       setContactSelected={setContactSelected}
+     
       ></Nav>
       <main>
-          <ContactForm></ContactForm>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          {/* <PhotoList/> */}
-          <About></About>
+      {!contactSelected ? (
+  <>
+    <Gallery currentCategory={currentCategory}></Gallery>
+    <About></About>
+  </>
+) : (
+    <ContactForm></ContactForm>
+  )}
       </main>
     </div>
   );
